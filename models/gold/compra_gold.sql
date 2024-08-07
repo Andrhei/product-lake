73f9c13f07022e78fb6f733f8f3f9b2c54df50b3
Andrhei Silva <andrheihqs@gmail.com>
2024-08-07 23:01:05 +0000

    lasts updates

--- models/gold/compra_gold.sql
+++ models/gold/compra_gold.sql
@@ -1,7 +1,11 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='incremental',
+    unique_key='id'
+) }}
 
 WITH compra_gold AS (
     SELECT
+        id,
         nome_cliente,
         nacionalidade_cliente,
         data_vencimento,
@@ -16,3 +20,13 @@ WITH compra_gold AS (
     FROM {{ref('compra_silver')}}
 )
 SELECT * FROM compra_gold
+
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- (uses >= to include records whose timestamp occurred since the last run of this model)
+  -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
+where data_atualizacao_compra >= (select coalesce(max(data_atualizacao_compra),'1900-01-01') from {{ this }} )
+
+{% endif %}
